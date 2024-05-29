19cb19cc3ead6cca36fd80dc709b0f8d7fbb59d4
Juan Ludevid <jmludevid@hotmail.com>
2024-05-29 20:46:26 +1000

    update AuthProvider

--- src/config/api/tesloApi.ts
+++ src/config/api/tesloApi.ts
@@ -1,6 +1,8 @@
+/* eslint-disable dot-notation */
 import {Platform} from 'react-native';
 import axios from 'axios';
 import {STAGE, API_URL_ANDROID, API_URL_IOS, API_URL as PROD_URL} from '@env';
+import {StorageAdapter} from '../adapters/storage-adapter';
 
 export const API_URL =
   STAGE === 'prod'
@@ -16,4 +18,11 @@ const tesloApi = axios.create({
   },
 });
 
+tesloApi.interceptors.request.use(async config => {
+  const token = await StorageAdapter.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
 export {tesloApi};
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,25 @@
 /* eslint-disable react-native/no-inline-styles */
 import {Layout, Text, Button, Icon} from '@ui-kitten/components';
 import React from 'react';
+import {useAuthStore} from '../../store/auth/useAuthStore';
 
 export const HomeScreen = () => {
+  const {logout} = useAuthStore();
   return (
     <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>HomeScreen</Text>
 
-      <Button accessoryLeft={<Icon name="facebook" />}>End Session</Button>
+      <Button
+        accessoryLeft={
+          <Icon
+            name="log-out-outline"
+            onPress={() => {
+              logout;
+            }}
+          />
+        }>
+        End Session
+      </Button>
     </Layout>
   );
 };
